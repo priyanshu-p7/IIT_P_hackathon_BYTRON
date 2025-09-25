8c575f650ed6e7934a3015e24b4ffdbe9b3f99bf
Priyanshu Kumar Prasad <priyanshukumar94460@gmail.com>
2025-09-26 00:03:50 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/IIT_P_hackathon_BYTRON/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
